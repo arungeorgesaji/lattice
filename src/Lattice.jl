fb24dabd0d106d17d994b01d4ba870a76339c9f6
arungeorgesaji <arungeorgesaji@gmail.com>
2025-11-07 12:18:11 +0530

    Forgot to commit for a while so individually added all the functionality in seperate commits as required to make the timeline proper(this one just modifies the main file to export the right things with the recently added functionality)

--- src/Lattice.jl
+++ src/Lattice.jl
@@ -2,8 +2,14 @@ module Lattice
 
 include("core/Grid.jl")
 include("core/operations.jl")
+include("core/transforms.jl")  
+include("domains/images.jl")
+include("domains/nn.jl")
 
 export Grid, zeros_grid, ones_grid, random_grid
 export map_grid, transform
+export convolve, blur_kernel, edge_detection_kernel, sharpen_kernel  
+export load_image, save_image, blur_image, sharpen_image, edge_detect_image
+export DenseLayer, ConvLayer, relu, sigmoid
 
 end
